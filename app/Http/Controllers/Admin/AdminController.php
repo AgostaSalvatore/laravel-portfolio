c2d58d05e47a510740c71b14381f939340f2957d
AgostaSalvatore <agosta.boolean@gmail.com>
2025-09-02 12:31:32 +0200

    feat: display authenticated user info in admin and profile pages

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,16 +4,19 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return 'Admin';
+        $user = Auth::user();
+        return 'Admin name: ' . $user->name;
     }
 
     public function profile()
     {
-        return 'Profile';
+        $user = Auth::user();
+        return 'Profile password: ' . $user->password;
     }
 }
