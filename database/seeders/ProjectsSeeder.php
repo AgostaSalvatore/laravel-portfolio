0de76a922fab5ac805bf0b2afec3ec67c8d11236
AgostaSalvatore <agosta.boolean@gmail.com>
2025-09-03 03:39:36 +0200

    feat: created projects table, project model and seed db with faker

---
+++ database/seeders/ProjectsSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Faker\Generator as Faker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProjectsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Faker $faker): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $newProject = new Project();
+
+            $newProject->title       = $faker->sentence(3);
+            $newProject->description = $faker->text(100);
+            $newProject->image       = $faker->imageUrl(640, 480);
+            $newProject->link        = $faker->url();
+
+            $newProject->save();
+        }
+    }
+}
