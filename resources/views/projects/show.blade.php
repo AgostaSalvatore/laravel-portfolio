585ea20effb6327c159d01065bb5af5d4ad94491
AgostaSalvatore <agosta.boolean@gmail.com>
2025-09-04 14:47:10 +0200

    feat: add confirmation modal before project deletion

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -7,12 +7,34 @@
     <a class=" py-3 m-3 btn btn-outline-primary" href="{{ route('projects.index') }}">Torna alla lista</a>
     <a class=" py-3 m-3 btn btn-outline-warning" href="{{ route('projects.edit', $project) }}">Modifica</a>
 
-    <form class="d-inline" action="{{ route('projects.destroy', $project) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit" class=" py-3 m-3 btn btn-outline-danger">Elimina</button>
-    </form>
+    <button type="button" class="py-3 m-3 btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">
+        Elimina
+    </button>
 
+    <!-- Modal per conferma eliminazione -->
+    <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="deleteModalLabel">Conferma Eliminazione</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Sei sicuro di voler eliminare definitivamente il progetto "<strong>{{ $project->title }}</strong>"?
+                    <br><br>
+                    <small class="text-muted">Questa azione non può essere annullata.</small>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                    <form class="d-inline" action="{{ route('projects.destroy', $project) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Elimina Definitivamente</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <h1> Title: {{ $project->title }}</h1>
     <p> Description: {{ $project->description }}</p>
